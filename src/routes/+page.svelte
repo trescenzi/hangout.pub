5fe5fb05bb08872a0f1fb9cc3f4fa9e8c49e1638
Thomas Crescenzi <trescenzi@gmail.com>
2023-06-22 21:47:47 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<main>
+  <h1>Hangout Publishing</h1>
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem 1rem;
+    height: 100%;
+  }
+</style>
