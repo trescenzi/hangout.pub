56c9608a3496e28aa1b40be3e58aca4c04c7017d
Thomas Crescenzi <trescenzi@gmail.com>
2023-06-27 21:52:15 -0400

    hookup discord faq form

--- src/routes/warp21/+page.server.ts
+++ src/routes/warp21/+page.server.ts
@@ -1,13 +1,19 @@
 import { redirect } from '@sveltejs/kit';
 export const actions = {
-    default: async ({ cookies, request }) => {
-        const data = await request.formData();
-        const email = data.get('question');
-        console.log(email);
+  default: async ({ cookies, request }) => {
+    const token = process.env.WARP21FAQ_BOT_TOKEN;
+    const channelId = process.env.WARP21FAQ_CHANNEL_ID;
+    const data = await request.formData();
+    const question = data.get('question');
+    await fetch(`https://discord.com/api/channels/${channelId}/messages`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bot ${token}`,
+      },
+      body: JSON.stringify({content: question}),
+    });
 
-        //const user = await db.getUser(email);
-        //cookies.set('sessionid', await db.createSession(user));
-
-        throw redirect(303, '/warp21#faq'); 
-    },
+    throw redirect(303, '/warp21#faq'); 
+  },
 }
