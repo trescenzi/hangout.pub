27da64566a3572e02f3afe0b1be27d1b6012fd21
Thomas Crescenzi <trescenzi@gmail.com>
2023-07-17 11:53:06 -0500

    feat(css): transpile main.css with lightning css and bundle at the top

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import { dev } from '$app/environment';
   import { inject } from '@vercel/analytics';
   import { page } from '$app/stores';
+  import '/src/main.css';
 
   inject({ mode: dev ? 'development' : 'production' });
 </script>
